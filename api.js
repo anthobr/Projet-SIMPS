fd1d07dfe320855a06090d5d4fd1ca7ccd99d266
anthobr <tonybruckert1@gmail.com>
2023-03-26 16:22:19 +0200

    ninth commit

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 
-const myButton = document.querySelector('#dropMenu');
+/*const myButton = document.querySelector('#dropMenu');
 const imgContainer = document.querySelector('.imgContainer');
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -35,7 +35,7 @@ fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?count=20`)
   .catch((error) => {
       console.error('Erreur lors de l\'appel API:', error);
   });
-});
+});*/
 
 
 /*// je cible mes éléments sur le DOM 
@@ -43,7 +43,6 @@ const characterList = document.getElementById("dropMenu");
 const characterView = document.getElementById("container");
 
 // ref de connexion Api
-const characterIds = [333, 76, 74, 79, 80, 184, 71, 68, 72, 73, 67];
 const baseUrl = "https://api.sampleapis.com/simpsons/characters/";
 
 
@@ -58,6 +57,83 @@ characterIds.forEach(id => {
       option.textContent = character.normalized_name;
       option.textContent = character.gender;
       option.textContent = character.name;
+    
+  // Créer un élément img avec l'URL de l'image du personnage
+    const img = document.createElement("img");
+    img.src = `/images/${character.name}.jpg`; // ou un autre format d'image
+    img.alt = character.name;
+    img.width = 50; // ou une autre taille
+
+    // Ajouter l'élément img à l'option
+    option.appendChild(img);
+      characterList.appendChild(option); 
+    
+    })
+  
+    .catch(error => {
+      console.error(`Erreur lors de la récupération des informations du personnage ${id}: ${error}`);
+      console.log(characterList);
+    });
+});
+
+
+
+// Ajouter un eventListener sur la liste d'options pour afficher la vue character correspondante
+characterList.addEventListener("change", () => {
+  const characterId = characterList.value;
+
+  // Effectuer un second appel API Fetch() pour récupérer les informations sur le character sélectionné
+  fetch(baseUrl + characterId)
+    .then((response) => response.json())
+    .then((character) => {
+      // Afficher les informations du character dans la vue character
+      characterView.innerHTML = `
+        <div class="card col-6 offset-6 m-auto">
+          <h2>${character.name}</h2>
+          <p>${character.normalized_name}</p>
+          <p>${character.gender}</p>
+         
+        </div>
+      `;
+    })
+    .catch(error => {
+      console.error(`Erreur lors de la récupération des informations du personnage ${characterId}: ${error}`);
+    });
+});*/
+
+
+// je cible mes éléments sur le DOM 
+const characterList = document.querySelector("#dropMenu");
+const characterView = document.querySelector(".imgContainer");
+
+// ref de connexion Api
+
+fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?count=20`)
+  .then((response) => {
+      if (!response.ok) {
+          throw new Error("Erreur HTTP " + response.status);
+      }
+      return response.json();
+  })
+  .then((result) => {
+    const uniq = [...new Set(result)];
+        uniq.forEach((photo) => {
+          const box = document.createElement("div");
+          box.className = ".imgContainer";
+          const imgP = document.createElement("img");
+          const nameEl = document.createElement("p");
+          const quoteEl = document.createElement("p");
+          nameEl.innerHTML = photo.character;
+          quoteEl.innerHTML = photo.quote;
+          imgP.src = photo.image;
+          imgP.className = "w-100";
+          nameEl.className = "text-primary";
+          quoteEl.className = "fst-italic";
+          box.appendChild(imgP);
+          box.appendChild(nameEl);
+          box.appendChild(quoteEl);
+          imgContainer.appendChild(box);
+        });
     
   // Créer un élément img avec l'URL de l'image du personnage
     const img = document.createElement("img");
